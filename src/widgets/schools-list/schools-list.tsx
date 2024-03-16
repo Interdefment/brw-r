47e0ba2a2f94011188a80c1e0432e4d84bd7d81a
Евгений Григорьев <e.grigoriev@huskyjam.com>
2024-03-17 01:41:33 +0300

    фикс ошибок линтинга

--- src/widgets/schools-list/schools-list.tsx
+++ src/widgets/schools-list/schools-list.tsx
@@ -1,18 +1,6 @@
-import { MAGIC_SCHOOLS, MagicSchool, MagicSchoolCard } from 'src/entities/magic-school';
+import { MAGIC_SCHOOLS, MagicSchoolCard } from 'src/entities/magic-school';
 
 export const SchoolsList: React.FC = () => {
-  const listTemplate = (items: MagicSchool[]) => {
-    if (!items || items.length === 0) return null;
-
-    const list = items.map((school) => {
-      return (
-        <MagicSchoolCard key={school.type} school={school} />
-      );
-    });
-
-    return <div className='grid grid-nogutter'>{list}</div>;
-  };
-
   return (
     <div className='flex flex-wrap gap-4 after:flex-auto'>
       {MAGIC_SCHOOLS.map((school) => {
